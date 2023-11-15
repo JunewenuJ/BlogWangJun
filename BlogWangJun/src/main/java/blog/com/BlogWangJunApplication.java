7a1cad582d98293638a265adf2ba975172ab8d50
JunewenuJ <hadesw@gmail.com>
2023-11-15 10:33:30 +0900

    プロジェクトの作成

---
+++ BlogWangJun/src/main/java/blog/com/BlogWangJunApplication.java
@@ -0,0 +1,13 @@
+package blog.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWangJunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWangJunApplication.class, args);
+	}
+
+}
